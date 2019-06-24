321e98f27013606cc17abadb035e2b6fbd09a5c1
Manuel J. Thomsen <mjt@magnetix.dk>
2019-06-24 10:13:51 +0200

    Revert "Make site look fantastic"
    This reverts commit 4a4d1aaec9bf13d72aef89829c33bc4838fe18e9.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,3 @@
-// gatsby-node.js
-
-const path = require(`path`)
-
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -9,34 +5,3 @@ const path = require(`path`)
  */
 
 // You can delete this file if you're not using it
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
-  return graphql(`
-    {
-      allContentfulBlogPost {
-        edges {
-          node {
-            title
-            slug
-            contentful_id
-          }
-        }
-      }
-    }
-  `).then(result => {
-    result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
-      createPage({
-        path: node.slug,
-        component: path.resolve(`./src/templates/blog-post.js`),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          id: node.contentful_id,
-        },
-      })
-    })
-  })
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => {
-  return (
-    <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      {data.allContentfulBlogPost.edges.map(({ node }) => (
-        <div key={node.contentful_id}>
-          <Link to={`/${node.slug}`}>
-            <h2>{node.title}</h2>
-          </Link>
-          <Img fluid={node.heroImage.fluid} />
-          <p>{node.description.description}</p>
-        </div>
-      ))}
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-  )
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
 
 export default IndexPage
-
-export const query = graphql`
-  query {
-    allContentfulBlogPost {
-      edges {
-        node {
-          title
-          slug
-          description {
-            description
-          }
-          heroImage {
-            fluid {
-              ...GatsbyContentfulFluid
-            }
-          }
-          contentful_id
-        }
-      }
-    }
-  }
-`
