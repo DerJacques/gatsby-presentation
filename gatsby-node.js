a4e448a4aedcec3ee68d3f55ea332ec5cbb475a0
Manuel J. Thomsen <mjt@magnetix.dk>
2019-07-03 16:00:13 +0200

    Created an awesome blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+// gatsby-node.js
+
+const path = require(`path`)
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +9,34 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  return graphql(`
+    {
+      allContentfulBlogPost {
+        edges {
+          node {
+            title
+            slug
+            contentful_id
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          id: node.contentful_id,
+        },
+      })
+    })
+  })
+}
